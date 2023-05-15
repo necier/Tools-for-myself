439e63c2df83451015643f3022ac31cc33abb367
necier <37684178+necier@users.noreply.github.com>
2023-05-15 13:16:34 +0800

    Update myAntiDul.py

--- python/myAntiDul.py
+++ python/myAntiDul.py
@@ -1,6 +1,6 @@
-import platform
 import multiprocessing
 import subprocess
+import sys
 
 
 def findAllFile(base):
@@ -21,13 +21,37 @@ def getHashCode(file_path):
 def func(file_path, index):
     print('正在扫描第 {} 个文件'.format(index), end='\r')
     hashcode = getHashCode(file_path)
+    # print(hashcode)
     return [file_path, hashcode]
 
 
 if __name__ == '__main__':
-
-    current_platform = platform.system()
-    if current_platform == 'Windows':
+    multiprocessing.freeze_support()
+    # Module multiprocessing is organized differently in Python 3.4+
+    try:
+        # Python 3.4+
+        if sys.platform.startswith('win'):
+            import multiprocessing.popen_spawn_win32 as forking
+        else:
+            import multiprocessing.popen_fork as forking
+    except ImportError:
+        import multiprocessing.forking as forking
+    if sys.platform.startswith('win'):
+        class _Popen(forking.Popen):
+            def __init__(self, *args, **kw):
+                if hasattr(sys, 'frozen'):
+                    # We have to set original _MEIPASS2 value from sys._MEIPASS
+                    # to get --onefile mode working.
+                    os.putenv('_MEIPASS2', sys._MEIPASS)
+                try:
+                    super(_Popen, self).__init__(*args, **kw)
+                finally:
+                    if hasattr(sys, 'frozen'):
+                        if hasattr(os, 'unsetenv'):
+                            os.unsetenv('_MEIPASS2')
+                        else:
+                            os.putenv('_MEIPASS2', '')
+        forking.Popen = _Popen
 
         import tkinter as tk
         from tkinter import filedialog
@@ -45,6 +69,7 @@ if __name__ == '__main__':
 
         files_path = findAllFile(ddir)
         num_files = len(files_path)
+        print('该文件夹内共有 {} 个文件'.format(num_files))
         file_and_hash_raw = []
         for i in range(num_files):
             file_and_hash_raw.append(pool.apply_async(func=func, args=(files_path[i], i)))
@@ -61,16 +86,18 @@ if __name__ == '__main__':
 
             else:
                 sorted_file[file_and_hash[i][1]] = [file_and_hash[i][0], ]
+        dul_exists = False
         for key in sorted_file:
             if len(sorted_file[key]) < 2:
                 continue
             else:
+                dul_exists = True
                 print('以下文件是完全相同的：')
                 for x in sorted_file[key]:
                     print('\t{}'.format(x), end='\n')
-
+        pool.terminate()
+        if not dul_exists:
+            print('此文件夹中没有完全相同的文件')
         os.system('pause')
-
-
     else:
-        print('Only available in Windows terminal:(')
+        print('Only available in Windows:(')
